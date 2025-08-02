66788347186dab50c5305b885000ea23d99d189c
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2025-08-02 01:36:34 -0400

    crear un array para almacenar los nombres

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # challenge-amigo-secreto
 repositorio del challenge del amigo secreto #aluraLATAM
+
+## Tareas
+
+requirements:
+ * validateName: not empty input
+ * addName: insert the input into an array
+ * visualizeList: autoUpdate
+ * randomSorteo: sortearAmigo button, select a random friend from the list.
+
+## Declare and Initialize the list of friends
+`let friends = []`
--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+//input ID: amigo
+
+/**requirements
+ * validateName: not empty input
+ * addName: insert the input into an array
+ * visualizeList: autoUpdate
+ * randomSorteo: sortearAmigo button, select a random friend from the list.
+ */
+
+//global variables
+
+let friends = []
